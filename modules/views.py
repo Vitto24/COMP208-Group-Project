7834b0237fdc7034ae550e4f626ee51f466c5602
Owen Wells <owenwells41@gmail.com>
2026-02-19 16:14:41 +0000

    Added partial functionality to the moudles section

--- modules/views.py
+++ modules/views.py
@@ -1,9 +1,21 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import Module, Week
+from grades.models import Assignment
 
 def module_list(request):
-    return render(request, 'modules/module_list.html')
-
+    modules = Module.objects.all()
+    return render(request, 'modules/module_list.html', {'modules': modules})
 
 def module_detail(request, code):
-    return render(request, 'modules/module_detail.html', {'code': code})
+    # Grab the specific module
+    module = get_object_or_404(Module, code=code)
+    # Grab all assignments for this module
+    assignments = Assignment.objects.filter(module=module)
+    # Grab the weeks and pre-fetch the materials so it doesn't lag out
+    weeks = Week.objects.filter(module=module).prefetch_related('materials')
+    
+    return render(request, 'modules/module_detail.html', {
+        'module': module,
+        'assignments': assignments,
+        'weeks': weeks
+    })

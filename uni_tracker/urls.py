dd7f03775ecd287face43b7dde38d87d9cff5e66
tyrbujac <tyr.bujac@gmail.com>
2026-04-17 20:59:50 +0100

    Brand the admin site header

--- uni_tracker/urls.py
+++ uni_tracker/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 
+admin.site.site_header = "Uni Tracker Administration"
+admin.site.site_title = "Uni Tracker Admin"
+admin.site.index_title = "Welcome to Uni Tracker Admin"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('dashboard.urls')),

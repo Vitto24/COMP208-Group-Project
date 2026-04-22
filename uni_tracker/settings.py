bccdb9ce5962773739c2b3e681213992dac4d73f
tyrbujac <tyr.bujac@gmail.com>
2026-04-22 08:41:46 +0100

    Enable WhiteNoise Finders So Static Files Serve Without Collectstatic

--- uni_tracker/settings.py
+++ uni_tracker/settings.py
@@ -135,6 +135,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+WHITENOISE_USE_FINDERS = True
 
 # Use email to log in instead of username
 AUTHENTICATION_BACKENDS = ['accounts.backends.EmailBackend']
